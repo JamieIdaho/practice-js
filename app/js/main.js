733ef2e2bd05644ba19bb55003d1b0705e84329d
Jamie Gamblin <jamiemgamblin@gmail.com>
2015-08-17 19:18:08 -0600

    input fields

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,9 @@
 console.log('Angular Rocks', angular);
+
+function square(baseNumber) {
+  var answer = baseNumber * baseNumber;
+  console.log("The square of " + baseNumber + " is " + answer);
+  return answer;
+}
+
+square(25);
